179a79bed9ac12469b15d6007ec592ee233d623a
RyskAlTropic <ales.ryska@tropicsquare.com>
2026-01-14 14:56:07 +0100

    Fixed problem in logic during running TestAll.sh - SLOT24 wasn't cleaned properly before generating new keypair

--- examples/TestAll.sh
+++ examples/TestAll.sh
@@ -3,8 +3,15 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname "$0")" && pwd)"
 
+echo "-------------------------BUILD-------------------------------------"
 bash "$SCRIPT_DIR/Build.sh"
+echo "-------------------------GenerateRandom.sh-------------------------"
 bash "$SCRIPT_DIR/GenerateRandom.sh"
+echo "-------------------------StoreUserData.sh--------------------------"
 SLOT=60 bash "$SCRIPT_DIR/StoreUserData.sh"
+echo "-------------------------EraseKey.sh----------------------------"
+SLOT=24 bash "$SCRIPT_DIR/EraseKey.sh"
+echo "-------------------------GenerateKey.sh----------------------------"
 SLOT=24 bash "$SCRIPT_DIR/GenerateKey.sh"
+echo "-------------------------Sign.sh-----------------------------------"
 SLOT=24 MECH=ECDSA bash "$SCRIPT_DIR/Sign.sh"
