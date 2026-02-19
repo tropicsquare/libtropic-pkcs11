a9952a28525fcc2e0c918af575116fe359032add
andreondra <golasowski.o@gmail.com>
2026-02-19 12:45:33 +0100

    doc: fix comment format

--- src/pkcs11.c
+++ src/pkcs11.c
@@ -1053,7 +1053,7 @@ CK_RV C_GetAttributeValue(CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE hObject,
             }
 
             case CKA_LABEL:
-                // Pass dummy label with zero length.
+                /* Pass dummy label with zero length. */
                 pTemplate[i].ulValueLen = 0;
                 break;
 
